0793aa3a6b8e5a1cb78dc785e11d0333840f01c5
Marjan Goglani <91924395+marjangoglani@users.noreply.github.com>
2024-04-02 11:19:21 -0500

    player animation and movement complete
    So happy I got this done. It's done differently than the tutorial because I learned a better way of doing this from a precious tutorial :')

--- player.gd
+++ player.gd
@@ -6,3 +6,22 @@ func _physics_process(delta):
 	var direction = Input.get_vector("walk_left", "walk_right", "walk_up", "walk_down")
 	velocity = direction * speed 
 	move_and_slide()
+	animation_player()
+
+func animation_player():
+	# velocity[0] = x
+	# velocity[1] = y
+	if velocity[0] == 0.0 && velocity[1] == 0.0: # idle
+		%AnimatedSprite2D.play("idle")
+	if velocity[0] > 0.0: # walking right
+		%AnimatedSprite2D.play("sidewalk")
+		%AnimatedSprite2D.flip_h = true
+	if velocity[0] < 0.0: # walking left
+		%AnimatedSprite2D.play("sidewalk")
+		%AnimatedSprite2D.flip_h = false
+	if velocity[1] > 0.0: # walking down
+		%AnimatedSprite2D.play("downwalk")
+	if velocity[1] < 0.0: # walking up
+		%AnimatedSprite2D.play("upwalk")
+	
+		
