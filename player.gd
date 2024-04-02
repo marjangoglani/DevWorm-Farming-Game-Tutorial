b2477c85c76aa45f461807c09c15224d9faa1fb9
Marjan Goglani <91924395+marjangoglani@users.noreply.github.com>
2024-04-02 10:59:06 -0500

    jitter while walking diagonally fixed
    just had to turn stretch mode to canvas items from viewport :) reddit is so helpful lol

--- player.gd
+++ player.gd
@@ -4,5 +4,5 @@ var speed = 50
 
 func _physics_process(delta):
 	var direction = Input.get_vector("walk_left", "walk_right", "walk_up", "walk_down")
-	velocity = direction * speed
+	velocity = direction * speed 
 	move_and_slide()
