88e71fb9cdd0dab2fab40c69e657d30813d401d6
Marjan Goglani <91924395+marjangoglani@users.noreply.github.com>
2024-04-13 00:53:06 -0500

    Put seed on tilled soil
    Added the functionality to only add seeds on the tilled soil :D

--- world.gd
+++ world.gd
@@ -1,7 +1,12 @@
 extends Node2D
 
 @onready var tile_map : TileMap = $TileMap
-var tilled_soil_layer_num = 1
+# layer number is NOT the z index. It's the index in which
+# they appear in the list
+# if you hover over the name you can see the layer num
+var tilled_soil_layer_num = 4
+var environment_layer_num = 5
+var can_place_seeds_custom_data = "can_place_seeds"
 
 func _input(event):
 	if Input.is_action_just_pressed("Click"):
@@ -9,5 +14,13 @@ func _input(event):
 		var tile_mouse_pos = tile_map.local_to_map(mouse_position)
 		var source_id = 2 		# kinda hard coded, is there a way I can grab this num from tile_map?
 		var atlas_coord = Vector2i(11,1)
-		tile_map.set_cell(tilled_soil_layer_num, tile_mouse_pos, source_id, atlas_coord)
+		var tile_data : TileData = tile_map.get_cell_tile_data(tilled_soil_layer_num, tile_mouse_pos)
 		
+		if tile_data:
+			var can_place_seeds = tile_data.get_custom_data(can_place_seeds_custom_data)
+			if can_place_seeds:
+				tile_map.set_cell(environment_layer_num, tile_mouse_pos, source_id, atlas_coord)
+			else:
+				print("can't place here")
+		else:
+			print("no tile data")
